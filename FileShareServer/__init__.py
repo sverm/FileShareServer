50438b5cdb0dad6f43aae6498576799f45823619
Shiv Verma <shivverma1996@gmail.com>
2015-11-28 21:53:43 -0600

    allow for '..' in non top level directories

---
+++ FileShareServer/__init__.py
@@ -0,0 +1,1 @@
+from PathContainer import PathContainer
--- FileShareServer/__main__.py
+++ FileShareServer/__main__.py
@@ -1,26 +1,17 @@
 from flask import Flask, send_from_directory, render_template
-import os
+from PathContainer import PathContainer
 
 app = Flask(__name__)
-
-def get_dir_info(realPath, path):
-    all_paths = os.listdir(realPath) # get all the directory contents' names
-    all_paths = [os.path.join(path,child_path) for child_path in all_paths] #get the full relative path
-    all_paths = [os.path.join(child_path,'') #add a '/' after the directories
-            if os.path.isdir(child_path)
-            else child_path
-            for child_path in all_paths]
-    return {'all_paths' : all_paths}
 
 @app.route('/', defaults={'path': ''}, methods=["GET"])
 @app.route('/<path:path>', methods=["GET"])
 def home_test(path):
-    realPath = os.path.realpath(path)
-    if os.path.isfile(realPath):
-        return send_from_directory('/',realPath[1:], as_attachment=True)
-    elif os.path.isdir(realPath):
-        return render_template('dirlist.html', **get_dir_info(realPath, path))
-    return realPath, 200
+    pc = PathContainer(path)
+    if pc.isfile: 
+        return send_from_directory('/',pc.real_path[1:], as_attachment=True)
+    elif pc.isdir:
+        return render_template('dirlist.html', all_children=pc.get_dir_contents())
+    return pc.real_path, 200
 
 if __name__ == "__main__":
     app.run(debug=True)
