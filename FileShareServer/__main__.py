4da62cef0e3bcda380bfc62e74d550e8328c7dea
Shiv Verma <shivverma1996@gmail.com>
2015-11-29 00:09:41 -0600

    make uploads work

--- FileShareServer/__main__.py
+++ FileShareServer/__main__.py
@@ -1,11 +1,12 @@
-from flask import Flask, send_from_directory, render_template, abort
+from flask import Flask, send_from_directory, render_template, abort, request, redirect, url_for
 from PathContainer import PathContainer
+from werkzeug import secure_filename
+import os
+
 
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''}, methods=["GET"])
-@app.route('/<path:path>', methods=["GET"])
-def home_test(path):
+def handle_get(path):
     pc = PathContainer(path)
     if pc.isfile: 
         return send_from_directory('/',pc.real_path[1:], as_attachment=True)
@@ -13,5 +14,19 @@ def home_test(path):
         return render_template('dirlist.html', all_children=pc.get_dir_contents())
     return abort(400)
 
+def handle_post(path, request):
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    uploaded_file.save(os.path.join(path,filename))
+    return redirect(url_for('home_test', path=path))
+
+@app.route('/', defaults={'path': ''}, methods=["GET", "POST"])
+@app.route('/<path:path>', methods=["GET", "POST"])
+def home_test(path):
+    if request.method == "POST":
+        return handle_post(path, request)
+    else:
+        return handle_get(path)
+    
 if __name__ == "__main__":
     app.run(debug=True)
