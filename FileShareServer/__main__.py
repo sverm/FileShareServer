c652f9e5419a5aba2766146a04761c802292a50e
Shiv Verma <shivverma1996@gmail.com>
2015-11-28 20:59:50 -0600

    force user to download files, append a '/' after the directories and fix a bug where user couldn't go more than one level deep in the directory structure

--- FileShareServer/__main__.py
+++ FileShareServer/__main__.py
@@ -3,18 +3,23 @@ import os
 
 app = Flask(__name__)
 
-def get_dir_info(real_dir_path):
-    all_contents = os.listdir(real_dir_path)
-    return {'all_contents' : all_contents, 'dir_path':real_dir_path}
+def get_dir_info(realPath, path):
+    all_paths = os.listdir(realPath) # get all the directory contents' names
+    all_paths = [os.path.join(path,child_path) for child_path in all_paths] #get the full relative path
+    all_paths = [os.path.join(child_path,'') #add a '/' after the directories
+            if os.path.isdir(child_path)
+            else child_path
+            for child_path in all_paths]
+    return {'all_paths' : all_paths}
 
 @app.route('/', defaults={'path': ''}, methods=["GET"])
 @app.route('/<path:path>', methods=["GET"])
 def home_test(path):
     realPath = os.path.realpath(path)
     if os.path.isfile(realPath):
-        return send_from_directory('/',realPath[1:])
+        return send_from_directory('/',realPath[1:], as_attachment=True)
     elif os.path.isdir(realPath):
-        return render_template('dirlist.html', **get_dir_info(realPath))
+        return render_template('dirlist.html', **get_dir_info(realPath, path))
     return realPath, 200
 
 if __name__ == "__main__":
